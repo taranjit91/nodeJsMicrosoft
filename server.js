1e17d8c4485c8b4f1a9213c4c05a29736404f6f3
developer <developer@pr-D204-Intern1.cencol.ca>
2017-07-05 15:21:08 -0400

    google maps integrated

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-var http = require("http");
-http.createServer(function(request, response) {
-    // Send the HTTP header 
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
+// var http = require("http");
+// http.createServer(function(request, response) {
+//     // Send the HTTP header 
+//     // HTTP Status: 200 : OK
+//     // Content Type: text/plain
+//     response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
-}).listen(8081);
+//     // Send the response body as "Hello World"
+//     response.end('Hello World\n');
+// }).listen(8081);
+// var app = require('express')(); //to install express write "npm install express"
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+// app.get('/testmap', function(req, res) {
+
+//     res.sendFile(path.join(__dirname + '/testMap.html'));
+// });
+// // Console will print the message
+// console.log('Server running at http://127.0.0.1:8081/');
+
+var express = require('express');
+var app = express();
+var path = require("path");
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/mapUsingGMAPSlib.html'));
+});
+app.listen(3000);
